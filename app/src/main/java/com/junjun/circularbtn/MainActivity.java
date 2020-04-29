e2b60b99fbcd9536af5c71885300a92af07a8511
dreamjun <906654918@qq.com>
2020-04-29 15:19:36 +0800

    vv的提交

--- app/src/main/java/com/junjun/circularbtn/MainActivity.java
+++ app/src/main/java/com/junjun/circularbtn/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 btn.setClick(true);
+                //这是vvv的分支
                 //测试提交
                 //测试提交
                 //测试提交
